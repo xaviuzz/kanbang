5a2a87b6e7fa4ccf23cf095784d9853330e316f4
Xavi Gost Bellver <xavi.gost@sngular.com>
2023-03-29 19:49:53 +0200

    starts with a empty basic kanban

--- src/components/kanban/column/Column.tsx
+++ src/components/kanban/column/Column.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './column.css'
 
 interface ColumnProps {
   name:string
 }
 
 const Column:React.FC<ColumnProps> = ({name})=>{
-  return <div role='column'><h1>{name}</h1></div>
+  return <div role='column' className='column'><h1>{name}</h1></div>
 }
 
 export default Column
