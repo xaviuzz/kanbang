9646d552de45535f99056c44eb37ed5c9952538e
Xavi Gost Bellver <xavi.gost@sngular.com>
2023-04-03 13:30:48 +0200

    trying to understand reactivity, some refactor

--- src/components/kanban/Kanban.tsx
+++ src/components/kanban/Kanban.tsx
@@ -4,31 +4,53 @@ import './kanban.css'
 import Cards from '../../domain/cards'
 import { CardDescription } from '../../domain/types'
 type ColumnDescription ={
+  id: string,
   name: string,
   content: Cards
 }
 
 const Kanban:React.FC = ()=>{
   const content:Array<ColumnDescription>=[
-    {name: 'to-do',content:new Cards()},
-    {name: 'doing',content:new Cards()},
-    {name: 'done',content:new Cards()}
+    {id: '1', name: 'to-do',content:new Cards()},
+    {id: '2', name: 'doing',content:new Cards()},
+    {id: '3', name: 'done',content:new Cards()}
   ]
 
   const[columns,setColumns]=useState<Array<ColumnDescription>>(content)
 
   const move =(from:string , id:string)=>{
     console.log(`moving from ${from} the card ${id}`)
-    const found = columns.find((column)=>{return column.name == from})
-    if (!found) return
-    const fromColumn:ColumnDescription = found as ColumnDescription
-    const indexFrom:number = columns.indexOf(fromColumn!)
-    const theCard:CardDescription = fromColumn.content.retrieve(id)
-    const indexTo:number = indexFrom+1
-    if(indexTo > columns.length-1) return
-    const newContent:Array<ColumnDescription>= Array.from(columns)
-    newContent[indexTo].content = newContent[indexTo].content.add(theCard)
-    setColumns(newContent)
+    
+    const theCard:CardDescription = retrieveCard(from,id)
+    const indexTo:number = getColumnPosition(from)+1
+
+    if(indexTo < columns.length) {
+      setColumns(addCardToColumn(indexTo,theCard))
+    }
+  }
+
+  const addCardToColumn=(indexTo:number,card:CardDescription)=>{
+    const newContent:Array<ColumnDescription>= [...columns]
+    newContent[indexTo].content = newContent[indexTo].content.add(card)
+    newContent[indexTo].name = 'XXXX'
+    return newContent
+  }
+
+  const getColumnPosition=(name:string):number=>{
+    const theColumn:ColumnDescription = getColumnByName(name)
+    return columns.indexOf(theColumn)
+  }
+
+  const getColumnByName=(name:string):ColumnDescription=>{
+    const found = columns.find((column)=>{return column.name == name})
+    if (!found) throw Error
+    return found as ColumnDescription
+  }
+
+  const retrieveCard=(from:string,card:string):CardDescription=>{
+    const fromColumn:ColumnDescription = getColumnByName(from)
+    const theCard:CardDescription = fromColumn.content.retrieve(card)
+    return theCard
   }
 
   const changeIn = (target:string,content:Cards)=>{
@@ -42,7 +64,7 @@ const Kanban:React.FC = ()=>{
   return <span className='kanban'>
     {columns.map((column) =>(
       <Column  
-        key={column.name}
+        key={column.id}
         name={column.name}
         content={column.content}
         onMove={(id:string)=>{move(column.name,id)}}
