7b807982be544a1e4d3111f88805afbcc30fb0e0
Xavi Gost Bellver <xavi.gost@sngular.com>
2023-06-28 13:43:04 +0200

    :wrench: adjust deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import { configDefaults } from 'vitest/config'
 
 export default defineConfig({
+  base:'/kanbang/',
   plugins: [react()],
   server: {
     port: 4000,
